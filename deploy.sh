ccda268fbec0a80dcc6a85d4292dc9716361cd01
Syed Irfan <syed.i@idexcel.com>
2020-07-28 19:35:25 +0530

    deploy.sh2

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@ pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 
 # install necessary dependency for ecs-deploy
-add-apt-repository ppa:eugenesan/ppa
-apt-get update
-apt-get install jq -y
+sudo add-apt-repository ppa:eugenesan/ppa
+sudo apt-get update
+sudo apt-get install jq -y
 
 # install ecs-deploy
 curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | \
