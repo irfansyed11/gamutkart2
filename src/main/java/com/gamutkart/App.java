72f9f045223308365f66ffce2765c9a501cba358
Syed Irfan <48002646+irfansyed11@users.noreply.github.com>
2020-08-03 21:15:27 +0530

    Update App.java

--- src/main/java/com/gamutkart/App.java
+++ src/main/java/com/gamutkart/App.java
@@ -5,11 +5,13 @@ public class App
     public static void main( String[] args )
     {
 		int i;
+	    System.out.println( "Start print number:");
 // testing continuous buids in Jenkins
 		for(i=0;i<=40;i++)
 		{
 			i += 3;
         	System.out.println( "print number: " + i);
    		}
+	    System.out.println( "End print number:");
 	 }
 }
