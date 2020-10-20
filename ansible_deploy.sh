4e4aaf0ac3f8aa9d90c696f9c9708f144dfed328
Syed Irfan <syed.i@idexcel.com>
2020-10-20 14:22:02 +0530

    adding deployfiles

---
+++ ansible_deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Copy source files to penetratindev environment   
+echo "folder"
+echo $0
+part1="$(dirname "$0")"
+echo $part1
+part2="$(basename "$0")"
+echo $part2
+cd $part1
+export file_contents=`cat version.html`
+#export ansible_inventory=`cat env.txt`
+ansible_inventory=$(head -n 1 env.txt)
+if [[ ${ansible_inventory} != "" ]]
+then
+echo $ansible_inventory
+cd /home/ubuntu/cync-devops-ansible
+ansible-playbook playbooks/cashangular.yml -i "dynamic-inventory/$ansible_inventory" --extra-vars "cashapp_angular_version=$file_contents"
+else
+  echo  " ${ansible_inventory} env name is not passed "
+fi
+
